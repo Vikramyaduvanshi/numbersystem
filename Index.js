c09cdd79f04290f247c189ce5647c780b4b75a66
Vikram Yadav <Vy784147@gmail.com>
2024-11-18 17:58:55 +0530

    First_file_numbersystem

---
+++ Index.js
@@ -0,0 +1,17 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+
+  for(let v of number){
+if(v%2==0){
+    console.log("number is Even");
+}
+
+  }
+  for(let v of number){
+if(v%2 != 0){
+    console.log("number is Odd");
+}
+
+  }

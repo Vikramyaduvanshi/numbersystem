3a55220fa9637da690fb1a32ef694bd3eee74ee2
Vikram Yadav <Vy784147@gmail.com>
2024-11-26 15:42:44 +0530

    task management

---
+++ scheduling.js
@@ -0,0 +1,5 @@
+let task=["Running","Markt analyse","Assigmnts complted","Masai shedule check","cook the food"]
+task[0]="coding self study"
+task[1]="english paragraph";
+task[4]="check pending assigments"
+console.log(task);
